c344876f2fef49bde8d166ae2ad05a3d54e7eb2f
Removel <Removel0202@163.com>
2025-12-11 12:08:27 +0800

    Infrastructure

---
+++ src/main/java/com/removel/mms_be/MmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.removel.mms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmsBeApplication.class, args);
+    }
+
+}
